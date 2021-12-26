3b162f37cedb21cc4ca83180033aeab559da8055
designeralabi <bdlkrmalabi@gmail.com>
2021-12-25 16:41:17 -0800

    added varKeyword

---
+++ varKeyword.js
@@ -0,0 +1,7 @@
+// initialize a variable and then store in a value
+var name = "John Doe";
+
+// redeclare value
+name = "John Smith Doe";
+
+// usage of var again means re-intializng which simple means to (reset/clear/empty);
