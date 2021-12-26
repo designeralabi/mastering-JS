d468603cf9952c15b93ad43be08f86a9df7e7dc2
designeralabi <bdlkrmalabi@gmail.com>
2021-12-25 17:26:15 -0800

    added objects

---
+++ objects.js
@@ -0,0 +1,25 @@
+
+// objects filled with arrays 
+var bioData = {
+    numberOfCity: ['lagos', 'Anambra', 'benue', 'asaba'],
+    numberOfAge: [15, 18, 20, 35]
+};
+
+console.log(bioData.numberOfCity);
+
+
+// arrays of objects
+var collections = [
+    {
+        name: "smith",
+        country: "Nigeria",
+        religion: 'islam'
+    },
+    {
+        name: "sandra",
+        country: "canada",
+        religion: "christainity"
+    }
+];
+console.log(collections);
+// objects does not works with ordering unless if associated with arrays
