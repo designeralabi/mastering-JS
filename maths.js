d4c76f0598be8d5c1230642e0b58ef246ada0f42
designeralabi <bdlkrmalabi@gmail.com>
2021-12-27 18:47:59 -0800

    added math.js

---
+++ maths.js
@@ -0,0 +1,22 @@
+// declared a varaible
+const area = 7.7;
+const area2 = 7.3;
+
+/* the round method round first number before decimal 
+to it nearest whole number if second number is greater than 5 */
+console.log(Math.round(area));
+console.log(Math.round(area2));
+
+
+// round number to lower whole number 
+console.log(Math.floor(area));
+
+// round number to higher whole number 
+console.log(Math.ceil(area));
+
+
+// round number to lowest whole number 
+console.log(Math.trunc(area));
+
+// random number, generate random number between 0-1 
+console.log(Math.random());
