e48e4870e778bfefe3e610fb4b96b81eb6195bf1
designeralabi <bdlkrmalabi@gmail.com>
2022-01-06 22:56:45 +0100

    + comment change in constructor

--- constructor.js
+++ constructor.js
@@ -1,3 +1,4 @@
+// create an object constructor
 function Hotel(name, room, booked){
     this.name = name;
     this.room = room;
@@ -7,8 +8,13 @@ function Hotel(name, room, booked){
     };
 }
 
+// create an instance of the Hotel 
 let hotel1 = new Hotel('celina', 100, 30);
 console.log(`the total number of room avaliable in ${hotel1.name} hotel is ${hotel1.roomCurrentAvailable()}`);
 
 let hotel2 = new Hotel('mabilah', 120, 40);
 console.log(`the total number of room avaliable in ${hotel2.name} hotel is ${hotel2.roomCurrentAvailable()}`);
+
+
+// delete property & value from hotel2 instance
+// delete hotel2.name
