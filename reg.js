3c8948aa9188ca97f5f25af6310306339b5045a4
designeralabi <bdlkrmalabi@gmail.com>
2022-02-22 01:08:49 +0100

    added example1

--- reg.js
+++ reg.js
@@ -1,5 +1,12 @@
-let waldoIsHiding = 'Somewhere Waldo is hiding in this text';
+let waldoIsHiding = "Somewhere Waldo is hiding in this text";
 
 let waldoRegex = /Waldo/;
 let result = waldoRegex.test(waldoIsHiding);
 console.log(result);
+
+//example 1
+let petString = "james has a pet cat.";
+let petRegex = /dog|cat|bird|fish/;
+let result1 = petRegex.test(petString);
+
+console.log(result1);
