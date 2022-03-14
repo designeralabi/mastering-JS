175eb5bdd519c7b5d4a116f66e40139ecb15146f
designeralabi <bdlkrmalabi@gmail.com>
2022-03-15 00:05:34 +0100

    more on RegEx

--- reg.js
+++ reg.js
@@ -1,12 +1,35 @@
-let waldoIsHiding = "Somewhere Waldo is hiding in this text";
-
-let waldoRegex = /Waldo/;
-let result = waldoRegex.test(waldoIsHiding);
-console.log(result);
-
 //example 1
-let petString = "james has a pet cat.";
+// RegEx with either type checking
+let petString = "james has a pet fish.";
 let petRegex = /dog|cat|bird|fish/;
 let result1 = petRegex.test(petString);
 
 console.log(result1);
+
+//example 2
+let studentNum = "the student are 38 in numbers";
+let schoolNum = /ar|8|stu/;
+let schoolResult = schoolNum.test(studentNum);
+
+console.log(schoolResult);
+
+//RegEx Case ignore while matching
+let myString = "freeCodeCamp";
+let fccRegex = /FreeCODECamp/i;
+let result = fccRegex.test(myString);
+console.log(result);
+
+
+// pratice with the Match method;
+"Hello, World!".match(/Hello/);
+let ourStr = "Regular expressions";
+let ourRegex = /expressions/;
+
+let ourRegexResult = ourStr.match(ourRegex);
+//console.log(ourRegexResult);
+
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let rresult = extractStr.match(codingRegex);
+// Change this line
+console.log(rresult);
